10558c48acbf2ab4d7c9ed589c6d8cc8c0488adf
AlexBlackwell1 <agbwell2@gmail.com>
2024-11-23 10:26:40 -0500

    styled wishlists

--- src/pages/Wishlists.tsx
+++ src/pages/Wishlists.tsx
@@ -6,24 +6,32 @@ import {
   setDoc,
   doc,
   deleteDoc,
+  updateDoc,
 } from "firebase/firestore";
 import { db } from "../firebase";
 import Header from "../components/Header";
 import "./../css/cover.css";
 import { AuthContext } from "../context/AuthContext";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTrashAlt,
+  faEdit,
+  faSave,
+  faCancel,
+} from "@fortawesome/free-solid-svg-icons";
 interface Wishlist {
   id: string;
   name: string;
+  createdBy: string;
 }
-
 const Wishlists: React.FC = () => {
   const { user, signin } = useContext(AuthContext);
   const [wishlists, setWishlists] = useState<Wishlist[]>([]);
   const [newWishlistName, setNewWishlistName] = useState("");
+  const [editWishlistId, setEditWishlistId] = useState<string | null>(null);
+  const [editWishlistName, setEditWishlistName] = useState<string>("");
   const navigate = useNavigate();
   const itemsCollection = collection(db, "WishLists");
-
   useEffect(() => {
     const fetchWishlists = async () => {
       if (user) {
@@ -31,30 +39,49 @@ const Wishlists: React.FC = () => {
         const wishlists: Wishlist[] = querySnapshot.docs.map((doc) => ({
           id: doc.id,
           name: doc.data().name,
+          createdBy: doc.data().createdBy,
         }));
         setWishlists(wishlists);
       }
     };
     fetchWishlists();
   }, [user, itemsCollection]);
-
-  const handleCreateWishlist = async () => {
-    if (user && newWishlistName) {
-      await setDoc(doc(db, "WishLists", newWishlistName), {
-        name: newWishlistName,
-      });
-      setNewWishlistName("");
-      navigate(0);
-    }
+  const handleCreateWishlist = async (
+    e: React.KeyboardEvent | React.MouseEvent
+  ) => {
+    if (
+      (e instanceof KeyboardEvent && e.key !== "Enter") ||
+      !user ||
+      !newWishlistName
+    )
+      return;
+    const newWishlistRef = doc(db, "WishLists", newWishlistName);
+    await setDoc(newWishlistRef, {
+      name: newWishlistName,
+      createdBy: user.uid,
+    });
+    setNewWishlistName("");
+    navigate(0);
   };
-
   const handleDeleteWishlist = async (id: string) => {
     if (user) {
       await deleteDoc(doc(db, "WishLists", id));
       navigate(0);
     }
   };
-
+  const handleEditWishlist = (id: string, name: string) => {
+    setEditWishlistId(id);
+    setEditWishlistName(name);
+  };
+  const handleSaveEditWishlist = async (id: string) => {
+    if (user && editWishlistName) {
+      const wishlistRef = doc(db, "WishLists", id);
+      await updateDoc(wishlistRef, { name: editWishlistName });
+      setEditWishlistId(null);
+      setEditWishlistName("");
+      navigate(0);
+    }
+  };
   if (!user) {
     return (
       <div className="d-flex h-100 text-center text-white bg-dark">
@@ -65,15 +92,13 @@ const Wishlists: React.FC = () => {
             links={[{ to: "/wishlists", label: "Wishlists" }]}
           />
           <main className="px-3">
-            <h1>Wishlists</h1>
-            <p>Please sign in to view your wishlists.</p>
+            <h1>Wishlists</h1> <p>Please sign in to view your wishlists.</p>
             <button onClick={signin}>Sign In</button>
           </main>
         </div>
       </div>
     );
   }
-
   return (
     <div className="d-flex h-100 text-center text-white bg-dark">
       <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
@@ -82,16 +107,67 @@ const Wishlists: React.FC = () => {
           currentPage="/wishlists"
           links={[{ to: "/wishlists", label: "Wishlists" }]}
         />
-
         <main className="px-3">
           <h1>Wishlists</h1>
-          <ul>
+          <ul className="wishlist-list">
             {wishlists.map((wishlist) => (
-              <li key={wishlist.id}>
-                <Link to={`/wishlists/${wishlist.id}`}>{wishlist.name}</Link>
-                <button onClick={() => handleDeleteWishlist(wishlist.id)}>
-                  Delete
-                </button>
+              <li
+                key={wishlist.id}
+                className={
+                  editWishlistId === wishlist.id
+                    ? "wishlist-item-edit"
+                    : "wishlist-item"
+                }
+              >
+                {editWishlistId === wishlist.id ? (
+                  <input
+                    type="text"
+                    value={editWishlistName}
+                    onChange={(e) => setEditWishlistName(e.target.value)}
+                    onKeyUp={(e) =>
+                      e.key === "Enter" && handleSaveEditWishlist(wishlist.id)
+                    }
+                    className="wishlist-edit-input"
+                  />
+                ) : (
+                  <Link
+                    to={`/wishlists/${wishlist.id}`}
+                    className="wishlist-link"
+                  >
+                    {wishlist.name}
+                  </Link>
+                )}
+                <div className="wishlist-actions">
+                  {editWishlistId === wishlist.id ? (
+                    <>
+                      <FontAwesomeIcon
+                        icon={faSave}
+                        className="wishlist-item-icon save-icon"
+                        onClick={() => handleSaveEditWishlist(wishlist.id)}
+                      />
+                      <FontAwesomeIcon
+                        icon={faCancel}
+                        className="wishlist-item-icon cancel-icon"
+                        onClick={() => handleEditWishlist("", "")}
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <FontAwesomeIcon
+                        icon={faEdit}
+                        className="wishlist-item-icon edit-icon"
+                        onClick={() =>
+                          handleEditWishlist(wishlist.id, wishlist.name)
+                        }
+                      />
+                      <FontAwesomeIcon
+                        icon={faTrashAlt}
+                        className="wishlist-item-icon delete-icon"
+                        onClick={() => handleDeleteWishlist(wishlist.id)}
+                      />
+                    </>
+                  )}
+                </div>
               </li>
             ))}
           </ul>
@@ -99,13 +175,16 @@ const Wishlists: React.FC = () => {
             type="text"
             value={newWishlistName}
             onChange={(e) => setNewWishlistName(e.target.value)}
+            onKeyUp={(e) => e.key === "Enter" && handleCreateWishlist(e)}
             placeholder="Create new wishlist"
+            className="wishlist-input"
           />
-          <button onClick={handleCreateWishlist}>Create</button>
+          <button onClick={handleCreateWishlist} className="wishlist-button">
+            Create
+          </button>
         </main>
       </div>
     </div>
   );
 };
-
 export default Wishlists;
